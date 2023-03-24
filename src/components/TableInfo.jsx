a1561f44b77c8765aba4c723c1fe545b410bea41
francisco <franga19.alfaro@gmail.com>
2023-03-23 22:05:47 -0600

    agregando informacion en tablas dinamicas

--- src/components/TableInfo.jsx
+++ src/components/TableInfo.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
+import { AiFillDelete } from "react-icons/ai";
 
 function TableInfo(cardInfo) {
   return (
     <table className="table-auto border-2 w-auto">
       <thead>
         <tr className="bg-gray-300">
-          <th>Medical diagnostic</th>
+          <th>employee</th>
           <th>Application date</th>
-          <th>Medical unit</th>
           <th>Medical unit</th>
           <th>Doctor</th>
           <th>Days of coverage</th>
           <th>Start date</th>
           <th>End date</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
       {cardInfo && cardInfo.cardInfo  && cardInfo.cardInfo.map((item, index) => (
           <tr className="h-8 border-b-2" key={index}>
             <td>{item.employee}</td>
-            <td>{item.employee}</td>
-            <td>{item.employee}</td>
-            <td>{item.employee}</td>
-            <td>{item.employee}</td>
-            <td>{item.employee}</td>
-            <td>{item.employee}</td>
-            <td>{item.employee}</td>
+            <td>{item.startDay}</td>
+            <td>{item.medicalUnit}</td>
+            <td>{item.doctor}</td>
+            <td>{item.daysOff}</td>
+            <td>{item.startDay}</td>
+            <td>{item.endDay}</td>
+            <td className='flex row items-center cursor-pointer text-red-700 font-bold fill-red-700 '> <AiFillDelete/></td>
           </tr>
         ))}
       </tbody>
